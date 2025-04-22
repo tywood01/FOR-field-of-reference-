1b504731265cc6ef462bb5380dd8d3e5a364bf86
EthanLHeureux <142943704+EthanLHeureux@users.noreply.github.com>
2025-04-22 15:32:24 -0500

    fix model path.

--- kam/models.py
+++ kam/models.py
@@ -4,5 +4,5 @@ from django.db import models
 # Create your models here.
 class picture(models.Model):
     id = models.AutoField(primary_key=True)
-    image = models.ImageField(upload_to="media/")
+    image = models.ImageField(upload_to="images/")
     name = models.CharField(max_length=256, default="")
