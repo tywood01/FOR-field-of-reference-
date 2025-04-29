5e804ac2313dd5fcf067982e2544d8417f801744
Tytus Woodburn <tytus.woodburn@student.cune.edu>
2025-04-29 13:22:52 -0500

    Added Album mmodel

--- kam/models.py
+++ kam/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-# Create your models here.
+class Album(models.Model):
+    id = models.AutoField(primary_key=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256, default="")
+    description = models.TextField(default="")
+
+
 class Picture(models.Model):
     id = models.AutoField(primary_key=True)
     image = models.ImageField(upload_to="images/")
     name = models.CharField(max_length=256, default="")
+    album = models.ForeignKey(
+        Album, on_delete=models.CASCADE, null=True, blank=True, related_name="pictures"
+    )
